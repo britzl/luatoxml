8c36eaac81887bcb001e656ed61d6b08808da02a
Björn Ritzl <bjorn.ritzl@king.com>
2014-03-31 07:35:04 +0200

    Cleanup

--- src/luatoxml/luatoxml.lua
+++ src/luatoxml/luatoxml.lua
@@ -6,9 +6,9 @@ local indentation = ""
 --- Parse a lua value and transform it to XML
 -- If the value is of type string it will be transformed into a tag eg <value/>
 -- If the value is a table each of it's key value pairs will be converted like this:
---  * If the key is a number parsevalue will be called again with the value as argument
+--  * If the key is a number toxml() will be called again with the value as argument
 --  * If the key is a string and value is a string a tag will be created with the key as XML tag element and the value as XML-value (eg <key>value</key>)
---  * If the key is a string and value is a table a tag will be created with the key as XML tag element and the table values as XML-attributes and nested tags (eg <key a="b"><c... </key> 
+--  * If the key is a string and value is a table a tag will be created with the key as XML tag element and the table values as XML-attributes and nested tags (eg <key a="b"><c... </key>
 -- @param value The value to parse
 -- @return The value as XML
 local function toxml(value)
@@ -22,8 +22,8 @@ local function toxml(value)
 			if type(name) == "number" then
 				xml = xml .. toxml(data)
 			else
-				xml = xml .. indentation .. "<" .. name				
-				if type(data) == "table" then	
+				xml = xml .. indentation .. "<" .. name
+				if type(data) == "table" then
 					local children = {}
 					local number_strings = {}
 					for k,v in pairs(data) do
@@ -37,8 +37,8 @@ local function toxml(value)
 							table.insert(number_strings, v)
 						end
 					end
-					
-					if next(children) ~= nil or #number_strings > 1 then	
+
+					if next(children) ~= nil or #number_strings > 1 then
 						xml = xml .. ">\n"
 						indentation = indentation .. "\t"
 						xml = xml .. toxml(children)
@@ -53,8 +53,8 @@ local function toxml(value)
 						end
 					end
 				else
-					xml = xml .. ">"..data.."</" .. name ..">\n"	
-				end				
+					xml = xml .. ">"..data.."</" .. name ..">\n"
+				end
 			end
 		end
 	end
